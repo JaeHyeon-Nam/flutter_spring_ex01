436b94d7ec5f709f2b9b67f5f719e30b910009cc
JaeHyeon <njonhj@naver.com>
2022-06-20 01:06:24 +0900

    update

---
+++ fluttter_ex01/lib/dto/serviceinfo.dart
@@ -0,0 +1,19 @@
+class ServiceInfo{
+  const ServiceInfo({required this.name,required this.running});
+
+  final String name;
+
+  final bool running;
+
+  String status(){
+    if(running){
+      return "running";
+    }
+
+    return "stopped";
+  }
+
+  factory ServiceInfo.fromJson(Map<String, dynamic> json){
+    return ServiceInfo(name: json['name'],running: json['runnning']);
+  }
+}
--- fluttter_ex01/lib/main.dart
+++ fluttter_ex01/lib/main.dart
@@ -1,59 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'dart:async';
+import 'package:ex01/dto/serverinfo.dart';
+import 'package:ex01/dataservice.dart';
+import 'package:ex01/startpage.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  MyApp({required Key key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'Startup Name Generator',
-      home : RandomWords()
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class RandomWordsState extends State<RandomWords>{
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  late Future<List<ServerInfo>> futureData;
 
-  Widget _buildSuggestions(){
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context,i){
-          if(i.isOdd) return Divider();
-          final index = i ~/ 2;
-          if(index >= _suggestions.length){
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair){
-    return ListTile(
-      title : Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-    );
+  @override
+  void initState() {
+    super.initState();
+    futureData = fetchServerInfo();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
+    return MaterialApp(
+      title: "Serverstatus monitor",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      body: _buildSuggestions(),
+      home: StartPage(title: "Server status monitor", serverInfo: futureData),
     );
   }
-
-
-}
-
-class RandomWords extends StatefulWidget{
-  @override
-  RandomWordsState createState() => RandomWordsState();
 }
